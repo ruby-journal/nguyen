5600635cf526e532e0a36e22423a78a63a043666
m0thman <dmitry@lebed.me>
2013-04-15 21:51:33 +0300

    Add checkboxes support

--- lib/nguyen/fdf.rb
+++ lib/nguyen/fdf.rb
@@ -5,9 +5,9 @@ module Nguyen
   # Straight port of Perl's PDF::FDF::Simple by Steffen Schwigon.
   # Parsing FDF files is not supported (yet).
   class Fdf
-    
+
     attr_reader :options
-    
+
     def initialize(data = {}, options = {})
       @data = data
       @options = {
@@ -20,7 +20,7 @@ module Nguyen
     # generate FDF content
     def to_fdf
       fdf = header
-      
+
       @data.each do |key, value|
         if Hash === value
           value.each do |sub_key, sub_value|
@@ -34,34 +34,42 @@ module Nguyen
       fdf << footer
       return fdf
     end
-    
+
     # write fdf content to path
     def save_to(path)
       File.write(path, to_fdf)
     end
-    
+
     protected
-    
+
     def header
       header = "%FDF-1.2\n\n1 0 obj\n<<\n/FDF << /Fields 2 0 R"
-      
+
       # /F
       header << "/F (#{options[:file]})" if options[:file]
       # /UF
       header << "/UF (#{options[:ufile]})" if options[:ufile]
       # /ID
       header << "/ID[" << options[:id].join << "]" if options[:id]
-      
+
       header << ">>\n>>\nendobj\n2 0 obj\n["
       return header
     end
 
     def field(key, value)
-      "<</T(#{key})/V" +
-        (Array === value ? "[#{value.map{ |v|"(#{quote(v)})" }.join}]" : "(#{quote(value)})") +
-        ">>\n"
+      value_string = case value
+                       when TrueClass
+                         '/1'
+                       when FalseClass
+                         '/0'
+                       when Array
+                         '[' + value.map { |v| "(#{quote(v)})" }.join + ']'
+                       else
+                         "(#{quote(value)})"
+                     end
+      "<<\n/T(#{key})\n/V #{value_string}\n>>\n"
     end
-    
+
     def quote(value)
       value.to_s.strip.
         gsub( /\\/, '\\' ).
@@ -69,7 +77,7 @@ module Nguyen
         gsub( /\)/, '\)' ).
         gsub( /\n/, '\r' )
     end
-    
+
     FOOTER =<<-EOFOOTER
 ]
 endobj
@@ -80,10 +88,10 @@ trailer
 >>
 %%EOF
 EOFOOTER
-    
+
     def footer
       FOOTER
     end
-    
+
   end
 end
