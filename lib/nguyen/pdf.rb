d621b705bb6881e00fdb383bd309a1e4b1c3f1df
Pavel Slabý <paul.slaby@seznam.cz>
2017-08-21 14:07:41 +0200

    Read PDF form fields as utf8

--- lib/nguyen/pdf.rb
+++ lib/nguyen/pdf.rb
@@ -14,13 +14,13 @@ module Nguyen
 
     protected
 
-      def read_fields
-        field_output = @pdftk.call_pdftk %Q("#{path}"), 'dump_data_fields'
-        @fields = field_output.split(/^---\n/).map do |field_text|
-          if field_text =~ /^FieldName: (\w+)$/
-            $1
-          end
-        end.compact.uniq
-      end
+    def read_fields
+      field_output = @pdftk.call_pdftk path, 'dump_data_fields_utf8'
+      @fields = field_output.split("\n").map do |field_text|
+        if field_text =~ /^FieldName: (.+)$/
+          $1
+        end
+      end.compact.uniq
+    end
   end
 end
